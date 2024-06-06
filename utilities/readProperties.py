fcb0387ddd6320dfdba344d5dad7857810d2857a
Rahul Pawar <pwrahul@gmail.com>
2024-06-06 14:55:34 +0530

    Update readProperties.py
    updated config.read method with forward slash

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -1,7 +1,7 @@
 import configparser
 
 config = configparser.RawConfigParser()
-config.read(".\\Configurations\\config.ini")
+config.read("./Configurations/config.ini")
 
 class ReadConfig():
     @staticmethod
